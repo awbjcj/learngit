632643cb7a51232ad7c3e538d54f5bdefe0c8c64
awbjcj <wujiajin0303@gmail.com>
2021-12-31 13:25:51 -0500

    multiply

--- myfirstproject.py
+++ myfirstproject.py
@@ -1,1 +1,23 @@
-print("hello world")
+def mul(x, *nums):
+    p = x
+    for n in nums:
+         p = p * n
+    return p
+print('mul(5) =', mul(5))
+print('mul(5, 6) =', mul(5, 6))
+print('mul(5, 6, 7) =', mul(5, 6, 7))
+print('mul(5, 6, 7, 9) =', mul(5, 6, 7, 9))
+if mul(5) != 5:
+    print('测试失败!')
+elif mul(5, 6) != 30:
+    print('测试失败!')
+elif mul(5, 6, 7) != 210:
+    print('测试失败!')
+elif mul(5, 6, 7, 9) != 1890:
+    print('测试失败!')
+else:
+    try:
+        mul()
+        print('测试失败!')
+    except TypeError:
+        print('测试成功!')
