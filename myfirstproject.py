14c1219b25c06ecc22c673dedf367b13ad3cf420
awbjcj <wujiajin0303@gmail.com>
2022-01-02 02:01:45 -0500

    sorted 2

--- myfirstproject.py
+++ myfirstproject.py
@@ -141,7 +141,7 @@ def by_name(t):
     return t[0]
 L2 = sorted(L, key=by_name)
 print(L2)
-def by_score(t)
+def by_score(t):
     return t[1]
 L2 = sorted(L, key=by_score, reverse=True)
 print(L2)
