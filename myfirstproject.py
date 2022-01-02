91ecb971d8df0a20153d7e169e18f250beb93836
awbjcj <wujiajin0303@gmail.com>
2022-01-01 19:48:02 -0500

    add lbnz triangle

--- myfirstproject.py
+++ myfirstproject.py
@@ -30,7 +30,7 @@ else:
         move(1, a, b, c)
         move(n-1, b, a, c)
 move(4, "A", "B", "C")'''
-def trim(s):
+'''def trim(s):
     if s[:1] == " ":
         return trim(s[1:])
     elif s[-1:] == " ":
@@ -51,4 +51,39 @@ elif trim('') != '':
 elif trim('    ') != '':
     print('测试失败!')
 else:
-    print('测试成功!')
+    print('测试成功!')'''
+
+'''def triangles():
+    init = [1]
+    while True:
+        yield init
+        output = [init[i] + init[i+1] for i in range(len(init)-1)]
+        init = [1] + output + [1]
+
+
+n = 0
+results = []
+for t in triangles():
+    results.append(t)
+    n = n + 1
+    if n == 10:
+        break
+
+for t in results:
+    print(t)
+
+if results == [
+    [1],
+    [1, 1],
+    [1, 2, 1],
+    [1, 3, 3, 1],
+    [1, 4, 6, 4, 1],
+    [1, 5, 10, 10, 5, 1],
+    [1, 6, 15, 20, 15, 6, 1],
+    [1, 7, 21, 35, 35, 21, 7, 1],
+    [1, 8, 28, 56, 70, 56, 28, 8, 1],
+    [1, 9, 36, 84, 126, 126, 84, 36, 9, 1]
+]:
+    print('测试通过!')
+else:
+    print('测试失败!')'''
