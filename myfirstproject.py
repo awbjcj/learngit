9ecd2890057adb203ca0936076966469cace277e
awbjcj <wujiajin0303@gmail.com>
2022-01-02 02:01:13 -0500

    sorted

--- myfirstproject.py
+++ myfirstproject.py
@@ -126,7 +126,7 @@ if abs(str2float('114.514') - 114.514) < 0.00001:
 else:
     print('测试失败!')'''
 
-def is_palindrome(n):
+'''def is_palindrome(n):
     return str(n)== str(n)[::-1]
 
 output = filter(is_palindrome, range(1, 1000))
@@ -134,4 +134,14 @@ print('1~1000:', list(output))
 if list(filter(is_palindrome, range(1, 200))) == [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 22, 33, 44, 55, 66, 77, 88, 99, 101, 111, 121, 131, 141, 151, 161, 171, 181, 191]:
     print('测试成功!')
 else:
-    print('测试失败!')
+    print('测试失败!')'''
+
+L = [('Bob', 75), ('Adam', 92), ('Bart', 66), ('Lisa', 88)]
+def by_name(t):
+    return t[0]
+L2 = sorted(L, key=by_name)
+print(L2)
+def by_score(t)
+    return t[1]
+L2 = sorted(L, key=by_score, reverse=True)
+print(L2)
